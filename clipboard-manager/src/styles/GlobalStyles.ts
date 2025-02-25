4588955fa74a3aa79f044838af2e40334ec0c104
Renzo Caffesse <renzo.caffesse@gmail.com>
2025-02-25 00:38:08 -0300

    Added global styles and theme configuration

---
+++ clipboard-manager/src/styles/GlobalStyles.ts
@@ -0,0 +1,19 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  body {
+    font-family: ${(props) => props.theme.fonts.main};
+    background-color: ${(props) => props.theme.colors.background};
+    color: ${(props) => props.theme.colors.text};
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+`;
